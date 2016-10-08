437cb1f740bc3e61eafe35a5b6d5d0b6dfb18a60
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-10-08 14:16:43 -0300

    Explaining and organizing the code

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-originalImage = cv2.imread("source/TrainDatabase/1.jpg", 0)
-transformedImage = cv2.imread("source/TrainDatabase/1.jpg", 0)
+#1º Step: Get the images-------------------------------------------------------------
 
-#originalImage = cv2.imread("pout.tif", 0)
-#transformedImage = cv2.imread("pout.tif", 0)
+originalImage = cv2.imread('source/TrainDatabase/1.jpg', 0)
+transformedImage = cv2.imread('source/TrainDatabase/1.jpg', 0)
+
+#originalImage = cv2.imread('pout.tif', 0)
+#transformedImage = cv2.imread('pout.tif', 0)
+
+#2º Step: Normalize-------------------------------------------------------------
 
 #calcHist(images, channels, mask, histSize, ranges[, hist[, accumulate]])
 #The parameters are:
@@ -16,58 +20,74 @@ transformedImage = cv2.imread("source/TrainDatabase/1.jpg", 0)
 #histSize: Number of bits of the image
 #range: Range used to calculate
 
-histr = cv2.calcHist([originalImage],[0],None,[256],[0,256])
-#plt.plot(histr)
-#plt.show()
+#Plot of the histogram of the original image
+histrOriginalImage = cv2.calcHist([originalImage],[0],None,[256],[0,256])
+fig1 = plt.figure(1)
+fig1.suptitle('Histogram Comparison', fontsize=14, fontweight='bold')
+ax = fig1.add_subplot(111)
+ax.set_xlabel('x', fontweight='bold')
+ax.set_ylabel('y', fontweight='bold')
+plt.plot(histrOriginalImage)
 
+#Getting importants informations
 N = len(originalImage)
 M = len(originalImage[0])
 totalPixels = N*M
-histLen = len(histr)
-print type(histr)
-for i in range(histLen):
-    histr[i] = histr[i] / totalPixels
+histLen = len(histrOriginalImage)
 
-#plt.plot(histr)
-#plt.show()
+#Normalizing the histogram (The sum of the all elements will be 1)
+for i in range(histLen):
+    histrOriginalImage[i] = histrOriginalImage[i] / totalPixels
+
+plt.plot(histr)
+plt.show()
+
+#3º Step: Create a transformation function--------------------------------------
+
 L = 256 #Number of pixels
-s = np.zeros((histLen)) #Creating a vector with the same length of the histogram, to make a transformation function
+s = np.zeros((histLen)) #Creating a vector with the same length of the histogram
 initialValue = 1
+
 #Creating the transformation function 's'
 s[0] = np.rint( (L - 1) * histr[i] ) #Round to closest int number
 for i in range(initialValue, histLen):
     s[i] = np.rint( s[i-1] + (L - 1) * histr[i] )
 
-fig1 = plt.figure(2)
-fig1.suptitle('Transformation function',fontsize=14, fontweight='bold')
-ax = fig1.add_subplot(111)
-ax.set_xlabel('x', fontweight='bold')
-ax.set_ylabel('y', fontweight='bold')
-plt.plot(s)
-plt.show()
+#4º Step: Apply the transformation----------------------------------------------
 
 for i in range(N):
     for j in range(M):
         #Applying the transformation to the originalImage
         transformedImage[i][j] = s[originalImage[i][j]]
 
+#5º Step: Show the results------------------------------------------------------
 
-histr = cv2.calcHist([originalImage],[0],None,[256],[0,256])
-plt.plot(histr)
+#Plot of the histogram of the transformed image
+histrTransformedImage = cv2.calcHist([transformedImage],[0],None,[256],[0,256])
+plt.plot(histrTransformedImage)
 plt.show()
-histr = cv2.calcHist([transformedImage],[0],None,[256],[0,256])
-plt.plot(histr)
-plt.show()
-cv2.imshow("Transformed Image", transformedImage)
-cv2.imshow("Image", originalImage)
+
+#Plot of the transformation function
+fig2 = plt.figure(2)
+fig2.suptitle('Transformation function',fontsize=14, fontweight='bold')
+ax = fig2.add_subplot(111)
+ax.set_xlabel('x', fontweight='bold')
+ax.set_ylabel('y', fontweight='bold')
+plt.plot(s)
+
+#Opencv histogram equalization
+equOpencvImage = cv2.equalizeHist(originalImage)
+
+#Showing the images
+#imageComparison = np.concatenate((originalImage, transformedImage), axis=0) #another way to compare images
+imageComparison = np.hstack((originalImage, transformedImage, equOpencvImage))
+cv2.imshow('Images Comparison', imageComparison)
 cv2.waitKey(0)
 
-equ = cv2.equalizeHist(originalImage)
-res = np.hstack((transformedImage,equ)) #stacking images side-by-side
-cv2.imwrite('result.png',res)
+#-------------------------------------------------------------------------------
 
-
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-cl1 = clahe.apply(originalImage)
-res = np.hstack((originalImage,cl1))
-cv2.imwrite('clahe_2.jpg',res)
+#Otimization for histogram equalization
+# clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+# cl1 = clahe.apply(originalImage)
+# res = np.hstack((originalImage,cl1))
+# cv2.imwrite('clahe_2.jpg',res)
