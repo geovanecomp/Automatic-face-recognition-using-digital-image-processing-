b1d02a5cc4fe1321312ba37db8c67233e00ef1a9
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-11-02 20:34:59 -0200

    Image for fourier test added. Import from FourierTranform created on main class

--- main.py
+++ main.py
@@ -4,16 +4,20 @@ import cv2
 if __name__ == '__main__':
     from HistogramEqualization import *
     from LaplacianFilter import *
+    from FourierTransform import *
 
-    image = cv2.imread('Source/OthersImages/pout.tif', 0)
+    image = cv2.imread('Source/OthersImages/Cameraman.tif', 0)
     initialTime = time.time()
+    #
+    # histogramEqualization = HistogramEqualization(image)
+    # equalizedImage = histogramEqualization.calculate(True)
+    #
+    #
+    # laplacian = LaplacianFilter(equalizedImage)
+    # laplacian.laplacianFilter(True)
 
-    histogramEqualization = HistogramEqualization(image)
-    equalizedImage = histogramEqualization.calculate(True)
-
-
-    laplacian = LaplacianFilter(equalizedImage)
-    laplacian.laplacianFilter(True)
+    fourier = FourierTransform(image)
+    fourier.FourierTransform(1, 50, True)
 
 
     print 'Past time:', time.time() - initialTime
