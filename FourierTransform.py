28530e3e29eb8f56780e9d5cfa62461890159dd8
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-11-02 20:28:49 -0200

    Starting with de constructor's parameters

---
+++ FourierTransform.py
@@ -0,0 +1,19 @@
+# -*- coding: UTF-8 -*-
+import numpy as np
+import cv2
+import matplotlib.pyplot as plt
+import math
+
+#To not abbreviate big matrices
+np.set_printoptions(threshold='nan')
+
+class FourierTransform(object):
+    def __init__(self, image):
+        self.originalImage = np.float32(image)
+        self.M, self.N = np.shape(self.originalImage) #Number of lines and columns
+        self.P = 2 * self.M
+        self.Q = 2 * self.N
+        self.transformedImage = np.zeros((self.P, self.Q), dtype=np.float32)
+        for i in range(self.M):
+            for j in range(self.N):
+                self.transformedImage[i][j] = self.originalImage[i][j]
