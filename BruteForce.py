e14c334498235b1c604a2001bfb5264085cdcd74
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-11-10 19:37:23 -0200

    Starting the first face recognition method, the brute force

--- BruteForce.py
+++ BruteForce.py
@@ -8,4 +8,8 @@ np.set_printoptions(threshold='nan')
 
 class BruteForce(object):
     'This class will compare pixel by pixel the difference between the test image and the train images '
-    
+
+    def __init__(self, image):
+        self.originalImage = image
+        self.transformedImage = np.zeros(self.originalImage.shape, dtype=np.uint8)
+        self.M, self.N, self.O = self.originalImage.shape
