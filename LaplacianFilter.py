164171578fc9f74d14e13f476b6c29599643ade7
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-11-10 19:31:38 -0200

    Now the laplacian filter accept colorful images too

--- LaplacianFilter.py
+++ LaplacianFilter.py
@@ -12,7 +12,6 @@ class LaplacianFilter(object):
 
     def __init__(self, image):
         self.originalImage = np.float32(image)
-        self.lenImgX, self.lenImgY = np.shape(self.originalImage) #Number of lines and columns
 
 #Define the auxiliary functions-------------------------------------------------
 
@@ -51,27 +50,39 @@ class LaplacianFilter(object):
         cv2.imshow('Laplacian Filter: Original x Laplacian x Transformed Image', imageComparison)
         cv2.waitKey(0)
 
+        plt.figure(1)
+        plt.subplot(131),plt.imshow(self.originalImage, cmap = 'gray')
+        plt.title('Input image '), plt.xticks([]), plt.yticks([])
+        plt.subplot(132),plt.imshow(laplacian, cmap = 'gray')
+        plt.title('Mask'), plt.xticks([]), plt.yticks([])
+        plt.subplot(133),plt.imshow(transformedImage, cmap = 'gray')
+        plt.title('Filter applied'), plt.xticks([]), plt.yticks([])
+        plt.show()
+
 #Using the derivative approximation---------------------------------------------
 
     def __laplacianMask(self, f):
-        lenX, lenY = np.shape(f)
+        lenX, lenY, lenDim = np.shape(f)
+
         border = 1
-        g = np.zeros((lenX, lenY))
+        g = np.zeros((lenX, lenY, lenDim))
         for i in range(border, lenX - border):
             for j in range (border, lenY - border):
-                diagonalValues = f[i+1][j+1] + f[i-1][j+1] + f[i-1][j-1]+ f[i+1][j-1]
-                mainValues = f[i+1][j] + f[i-1][j] + f[i][j+1] + f[i][j-1]
-                centerValue = 8 * f[i][j]
+                for k in range(lenDim):
+                    diagonalValues = f[i+1][j+1][k] + f[i-1][j+1][k] + f[i-1][j-1][k] + f[i+1][j-1][k]
+                    mainValues = f[i+1][j][k] + f[i-1][j][k] + f[i][j+1][k] + f[i][j-1][k]
+                    centerValue = 8 * f[i][j][k]
 
-                value = mainValues + diagonalValues - centerValue
+                    value = mainValues + diagonalValues - centerValue
 
-                if value > 255:
-                    value = 255
+                    if value > 255:
+                        value = 255
 
-                if value < 0:
-                    value = 0
+                    if value < 0:
+                        value = 0
 
-                g[i][j] = value
+                    g[i][j][k] = value
+        print g.shape
         return g
 
 #Using the neighborhood method -------------------------------------------------
@@ -86,25 +97,27 @@ class LaplacianFilter(object):
         image = self.__addColumns(image, 2, 0)
         image = self.__addColumns(image, 2, len(image[0]))
 
+        M, N, O = image.shape
         g = np.zeros(image.shape, dtype=np.float32)
         a = (m-1)/2
         b = (n-1)/2
         value = 0
 
-        for x in range(len(image)-2):
-            for y in range(len(image[0])-2):
-                for s in range(len(w)):
-                    for t in range(len(w[0])):
-                        value = w[s][t]*image[x+s][y+t] + value
+        for x in range(M-2):
+            for y in range(N-2):
+                for k in range(O):
+                    for s in range(len(w)):
+                        for t in range(len(w[0])):
+                            value = w[s][t]*image[x+s][y+t][k] + value
 
-                if value < 0:
+                    if value < 0:
+                        value = 0
+
+                    if value > 255:
+                        value = 255
+
+                    g[x+1][y+1][k] = value
                     value = 0
-
-                if value > 255:
-                    value = 255
-
-                g[x+1][y+1] = value
-                value = 0
 
         g = self.__deleteRows(g, 2, 0)
         g = self.__deleteRows(g, 2, len(g))
@@ -119,22 +132,26 @@ class LaplacianFilter(object):
     def laplacianFilter(self, results=False):
 
         transformedImage = np.zeros(self.originalImage.shape, dtype=np.float32)
+        M, N, O = np.shape(self.originalImage)
+
         #I have made the laplacian filter using two forms
         laplacian = self.__laplacianMask(self.originalImage)
         #laplacian = self.__applyMask(self.originalImage)
 
         c = -1
 
-        for i in range(len(self.originalImage)):
-            for j in range(len(self.originalImage[0])):
-                value = self.originalImage[i][j] + c*laplacian[i][j]
-                if value < 0:
-                    value = 0
+        for i in range(M):
+            for j in range(N):
+                for k in range(O):
+                    value = self.originalImage[i][j][k] + c*laplacian[i][j][k]
 
-                if value > 255:
-                    value = 255
+                    if value < 0:
+                        value = 0
 
-                transformedImage[i][j] = value
+                    if value > 255:
+                        value = 255
+
+                    transformedImage[i][j][k] = value
 
         #Converting the images to unsigned int 8 bits
         self.originalImage = np.uint8(self.originalImage)
