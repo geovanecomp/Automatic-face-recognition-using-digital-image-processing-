85bdc4d4828cf69c0a693a0aed1993c97284dbd8
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-11-10 20:29:54 -0200

    Average function created

--- BruteForce.py
+++ BruteForce.py
@@ -9,7 +9,28 @@ np.set_printoptions(threshold='nan')
 class BruteForce(object):
     'This class will compare pixel by pixel the difference between the test image and the train images '
 
-    def __init__(self, image):
-        self.originalImage = image
-        self.transformedImage = np.zeros(self.originalImage.shape, dtype=np.uint8)
-        self.M, self.N, self.O = self.originalImage.shape
+    def __init__(self, image1, image2):
+        self.image1 = image1
+        self.image2 = image2
+
+        self.M, self.N, self.O = self.image1.shape
+
+    def __averageImage(self, image):
+        #Instead I could use np.average(image)
+        M, N, O = image.shape
+
+        sumOfElements = 0
+        numberElements = M*N*O
+
+        for i in range(M):
+            for j in range(N):
+                for k in range(O):
+                    sumOfElements += image[i][j][k]
+
+        average = sumOfElements / numberElements
+
+        print average, image.shape
+        return average, image.shape
+
+    def bruteForce(self):
+        self.__averageImage(self.image1)
