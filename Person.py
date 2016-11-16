42eb460b50604e10332493ddccd495544bf99020
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-11-16 20:24:12 -0200

    While I do not use a database

---
+++ Person.py
@@ -0,0 +1,17 @@
+# -*- coding: UTF-8 -*-
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+#To not abbreviate big matrices
+np.set_printoptions(threshold='nan')
+
+class Person(object):
+    'This class represents one person'
+
+    def __init__(self, directory, name, images, average=None):
+        self.directory = directory
+        self.name = name
+        self.images = images
+        self.average = average
