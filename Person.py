447909d7576044d7e1c5e196845a12973c027dc4
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-11-16 22:01:49 -0200

    getters and setters of person

--- Person.py
+++ Person.py
@@ -11,8 +11,31 @@ class Person(object):
     'This class represents one person'
 
     def __init__(self, directory, name, images, average=None):
-        self.directory = directory
-        self.name = name
-        self.images = images
-        self.average = average
- 
+        self.__directory = directory
+        self.__name = name
+        self.__images = images
+        self.__average = average
+
+    def setDirectory(self, directory):
+        self.__directory = directory
+
+    def getDirectory(self):
+        return self.__directory
+
+    def setName(self, name):
+        self.__name = name
+
+    def getName(self):
+            return self.__name
+
+    def setImages(self, images):
+        self.__images = images
+
+    def getImages(self):
+        return self.__images
+
+    def setAverage(self, average):
+        self.__average = average
+
+    def getAverage(self):
+        return self.__average
