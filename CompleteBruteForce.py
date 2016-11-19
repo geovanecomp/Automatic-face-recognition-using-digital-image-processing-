0e05da987f290db7737f9bf669f7e017a690b0ef
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-11-19 11:17:40 -0200

    Created the method to instantiate all train people to compare

--- CompleteBruteForce.py
+++ CompleteBruteForce.py
@@ -8,6 +8,8 @@ from Person import *
 
 #To not abbreviate big matrices
 np.set_printoptions(threshold='nan')
+
+#Constants
 URLTRAIN  = 'Source/Bernardo/TrainDatabase/'
 EXTENSION = '.jpg'
 DELIMITER = '_'
@@ -22,3 +24,28 @@ class CompleteBruteForce(object):
         avg = self.__averageImage(self.testImage)
         self.personTest = Person(directory=urlTestImage, name='unknown', images=self.testImage, average=avg)
         self.M, self.N, self.O = self.testImage.shape
+
+#-------------------------------------------------------------------------------
+
+    #Get all people to compare
+    def __getPeople(self):
+        #Count the number of "people".
+        #-1 its because this function count the TrainDatabase too
+        numberOfFolders = len(list(os.walk(URLTRAIN))) - 1;
+        people = [None] * numberOfFolders
+
+        for i in range(numberOfFolders):
+
+            #Getting the url, folders and files
+            directory, folders, files = os.walk(URLTRAIN+str(i+1)).next()
+
+            images = [None] * len(files)
+
+            for (j, file) in enumerate(files):
+                name, image = file.split(DELIMITER)
+                images[j] = image
+
+            person = Person(directory=directory, name=name, images=images)
+            people[i] = person
+
+        return people
