5f2764f71c86d3d6c7ce213ed05ca1ae5657dcfa
Geovane Pacheco <geovane.pacheco99@gmail.com>
2016-11-30 22:14:40 -0200

    Removing the mean of all elements

--- EigenFace.py
+++ EigenFace.py
@@ -54,3 +54,32 @@ class EigenFace(object):
                 faces.append(image.flatten())
 
         return faces
+def __averageVector(self, faces):
+    (M, N) = np.shape(faces)
+    average = np.zeros((N), dtype=np.float32)
+    for j in range(N):
+        for i in range(M):
+            average[j] += faces[i][j]
+        average[j] = average[j] / M
+
+    return average
+
+def __removeMean(self, faces, averageVector):
+    (M, N) = np.shape(faces)
+    newFaceMatrix = np.zeros((M,N), dtype=np.float32)
+    for i in range(M):
+        for j in range(N):
+            newFaceMatrix[i][j] = faces[i][j] - averageVector[j]
+
+    return newFaceMatrix
+
+
+#-------------------------------------------------------------------------------
+
+#The main method
+def eigenFaceMethod(self):
+    faces = self.__getFacesMatrix(self.__people)
+    averageVector = self.__averageVector(faces)
+    faces = self.__removeMean(faces, averageVector)
+
+    print faces
